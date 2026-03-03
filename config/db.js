259c8ef00e1269e22cf663f0e39e8e63ccb18841
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-03-03 11:11:37 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    return conn;
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    return connect;
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     return null;
