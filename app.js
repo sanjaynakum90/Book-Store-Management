e422679dbcdb536f3398cedbb829626908824b11
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-03-03 10:59:06 +0530

    update

--- app.js
+++ app.js
@@ -8,23 +8,25 @@ import connectDb from "./config/db.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-/* 🔥 IMPORTANT: Body Parsers */
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* Routes */
+
 app.use("/books", bookRoutes);
 
-app.get("/", (req, res) => res.redirect("/books"));
+app.get("/",(req,res)=>{
+  res.status(200).json({message:"hello form server"})
+})
 
-/* 404 Handler */
+
 app.use((req, res, next) => {
   next(new HttpError("Route not found", 404));
 });
 
-/* Global Error Handler */
+
 app.use((error, req, res, next) => {
   res.status(error.statusCode || 500).json({
     message: error.message || "Internal Server Error",
